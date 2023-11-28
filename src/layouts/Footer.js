e8dbcb862b5fe3efaf0f3db2dd9af0686ab4ffe1
ms <mateusz@>
2023-11-28 11:31:43 +0100

    done project SPA

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,9 +1,26 @@
+// Footer.js
 import React from 'react';
+import { Routes, Route, Outlet, Link, useParams } from 'react-router-dom';
 
-const Footer = () => {
-    return ( 
-        <div>STOPKA</div>
-     );
-}
- 
+const Home = () => (
+  <p>Jesteś na stronie głównej</p>
+);
+
+const Products = () => {
+  const { page } = useParams();
+  return (
+    <p>Jesteś na stronie {page}</p>
+  );
+};
+
+const Footer = () => (
+  <div>
+    <h2>Stopka</h2>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/produkty/:page' element={<Products />} />
+    </Routes>
+  </div>
+);
+
 export default Footer;
